7e0e53760dba00952b28b9b2c433dfcc58fbe9b8
viktorhex <christopherdillinger200@gmail.com>
2025-02-15 09:32:05 -0500

    update

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+import { roboto_mono } from '../../ui/fonts'
+
+export default function Customers(){
+  return <p className={roboto_mono.className}>Customers page</p>
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Roboto_Mono, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '400' });
